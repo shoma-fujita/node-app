907ba350b2ae4f2f2ca0eda0fb98f8ef6f63bbeb
sho-pc123 <syouteni123@gmail.com>
2024-04-08 08:36:13 +0900

    header情報の設定

---
+++ header.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+var server = http.createServer(
+  (request,response) => {
+    // header情報の設定
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<!DOCTYPE html><html lang="ja">');
+    response.write('<head><meta charset="utf-8">');
+    response.write('<title>Hello<title></head>');
+    response.write('<body><h1>Hello Node.js!</h1>');
+    response.write('<p>This is sample page!</p>');
+    response.write('<p>これは、Node.jsのサンプルページです</p>');
+    response.write('</body></html>');
+    resopose.end();
+  }
+);
+
+server.listen(3000);
+console.log('server start!');
