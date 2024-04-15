c9de8e4e7009b247ae80d28a77527c41ceee3af3
shoma-fujita <80296764+shoma-fujita@users.noreply.github.com>
2024-04-15 09:11:44 +0900

    Message board (#1)
    * 初期化
    
    * 初期化
    
    * データを表示する部分コンポーネントの作成
    
    * ログイン画面の作成
    
    * ejsのインストールとメインプログラム実装
    
    * 細かい修正

--- app.js
+++ app.js
@@ -1,15 +1,125 @@
-// モジュールローディングシステム
 const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+const url = require('url');
+const qs = require('querystring');
 
-var server = http.createServer(
-  // request: クライアントから送られてきた情報を管理するもの
-  // response: サーバーから送り出される情報を管理するもの
-  (request,response) => {
-    response.end('Hello Node.js!');
-  }
-);
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const login_page = fs.readFileSync('./login.ejs', 'utf8');
 
-// httpサーバーを待ち受け状態にする = ブラウザからポート番号を入力してアクセスすることができる
+// 最大保管数
+const max_num = 10;
+// データファイル名
+const filename = 'mydata.txt';
+// メッセージ格納用のデータ
+var message_data;
+readFromFile(filename);
+
+var server = http.createServer(getFromClient);
+
 server.listen(3000);
+console.log('Server start!');
 
-// 上記が「Node.jsで、サーバーを使って動かす」という「1,httppの用意 2,createServerでhttp.Serverの作成 3,待ち受け開始」の3つの手順
+// ここまでメインプログラム==========
+
+// createServerの処理
+function getFromClient(request, response) {
+
+  var url_parts = url.parse(request.url, true);
+  switch (url_parts.pathname) {
+
+    // トップページ（メッセージボード）
+    case '/':
+      response_index(request, response);
+      break;
+
+      // ログインページ
+    case '/login':
+      response_login(request, response);
+      break;
+
+    default:
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end('no page...');
+      break;
+  }
+}
+
+// loginのアクセス処理
+function response_login(request, response) {
+  var content = ejs.render(login_page, {});
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
+}
+
+// indexのアクセス処理
+function response_index(request, response) {
+  // POSTアクセス時の処理
+  if (request.method == 'POST') {
+    var body = '';
+
+    // データ受信のイベント処理
+    request.on('data', function (data) {
+      body += data;
+    });
+
+    // データ受信終了のイベント処理
+    request.on('end', function () {
+      data = qs.parse(body);
+      addToData(data.id, data.msg, filename, request);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+// indexのページ作成
+function write_index(request, response) {
+  var msg = "※何かメッセージを書いて下さい。";
+  var content = ejs.render(index_page, {
+    title: 'Index',
+    content: msg,
+    data: message_data,
+    filename: 'data_item',
+  });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
+}
+
+// テキストファイルをロード
+function readFromFile(fname) {
+  fs.readFile(fname, 'utf8', (err, data) => {
+    // 各行ごとにテキストを配列に格納する
+    message_data = data.split('\n');
+  })
+}
+
+// データを更新
+function addToData(id, msg, fname, request) {
+  var obj = { 'id': id, 'msg': msg };
+  // オブジェクトをテキストに変換する
+  var obj_str = JSON.stringify(obj);
+  console.log('add data: ' + obj_str);
+  // message_data配列の最初に格納zする
+  message_data.unshift(obj_str);
+  if (message_data.length > max_num) {
+    // message_data配列の最後の値を削除する
+    message_data.pop();
+  }
+  saveToFile(fname);
+}
+
+// データを保存
+function saveToFile(fname) {
+  // 配列を1つのテキストに変換する
+  var data_str = message_data.join('\n');
+  // データを保存する用のファイルに保存する
+  fs.writeFile(fname, data_str, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
